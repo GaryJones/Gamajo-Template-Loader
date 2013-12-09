5645be8db274db2e938b7697d3831a2866f7e1c2
Pippin Williamson <pippin@pippinsplugins.com>
2013-12-09 16:09:13 -0600

    Correct method name
    The `get_template_file_names` was renamed to `get_template_possible_parts` but then not updated in `get_template_part`. This fixes that.

--- class-gamajo-template-loader.php
+++ class-gamajo-template-loader.php
@@ -67,7 +67,7 @@ class Gamajo_Template_Loader {
 		do_action( 'get_template_part_' . $slug, $slug, $name );
 
 		// Get files names of templates, for given slug and name.
-		$templates = $this->get_template_file_names( $slug, $name );
+		$templates = $this->get_template_possible_parts( $slug, $name );
 
 		// Return the part that is found
 		return $this->locate_template( $templates, $load, false );
